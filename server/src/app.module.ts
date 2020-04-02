50856c8d456c126ddc918d8260401f7e3c81d9ce
godfreyyeung <godfreyyeung@gmail.com>
2020-04-02 14:52:48 -0400

    delete unnecessary imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
-import { ConfigService } from './config/config.service';
-import { ContactService } from './contact/contact.service';
-import { CrmService } from './crm/crm.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from './config/config.module';
 import { ContactModule } from './contact/contact.module';
@@ -13,6 +8,5 @@ import { CrmModule } from './crm/crm.module';
 @Module({
   imports: [AuthModule, ConfigModule, ContactModule, CrmModule],
   controllers: [AppController],
-  // providers: [AppService, AuthService, ConfigService, ContactService, CrmService],
 })
 export class AppModule {}
