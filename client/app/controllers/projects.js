d99b6a66b5e23e02554a0117c4f2e826844e452e
Brandyn Friedly <bfriedly@planning.nyc.gov>
2020-04-03 17:46:15 -0400

    Update UI for project lists
    This commit updates ui logic to better organize the projects template.  I started to split the project card components based on the ui, but learned from pairing with Andy how to simplify the card styling code, and a strategy for combining these into one component and effectively using ember parameters and conditional logic in the component templates.
    
    Next steps:
    - Write the business logic for filtering for “Upcoming” projects, ones that have no pas packege objects
    - Implement conditional UI logic for the project cards to show or not show buttons based on the package statuscode
    - Update the styling to match the figma wireframes (they’ve diverged a little bit since the github issue 17 was created).
    
    Remaining work on this issue:
    - Switch from dummy static data to api request
    - Impement breadcrumb component
    - Write tests

--- client/app/controllers/projects.js
+++ client/app/controllers/projects.js
@@ -6,17 +6,10 @@ export default class ProjectsController extends Controller {
         // handle filtering here, reference in template as this.applicantProjects
         // todo if statuscode is "Save" or "Package Preparation"
         return this.model.filter((project) => {
-            console.log('project being filtered', project)
-            // if (project.primaryApplicant === 'Brandyn Friedly') { return true}
-
             return project.packages.some((projectpackage) => {
                 if (projectpackage.statuscode === 'Saved' || projectpackage.statuscode === 'Package Preparation') {
-                    console.log("TRUEEE", project)
                     return true
-                    } else { 
-                        console.log('FAAALSE') 
-                        return false
-                    }
+                    } else { return false }
             })
 
         })
@@ -24,19 +17,17 @@ export default class ProjectsController extends Controller {
 
     // projects for nyc planning to do
     get planningProjects () {
-        // if statuscode is "Submitted", "Under Review", or "Revision Required"
-        console.log("planningProjects getter")
         return this.model.filter((project) => {
             return project.packages.some((projectpackage) => {
                 if (projectpackage.statuscode === 'Submitted' || projectpackage.statuscode === 'Under Review' || projectpackage.statuscode === "Revision Required") {
-                    console.log("TRUEEE", project)
                     return true
-                    } else { 
-                        console.log('FAAALSE') 
-                        return false
-                    }
+                    } else { return false }
             })
 
         })
+    }
+
+    get upcomingProjects () {
+        // TODO: write filter for projects with no pas package objects
     }
 }
