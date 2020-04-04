6d21d03a9e51418306a1c36d6aea5fc194f387fe
Matt Gardner <wilburnforce@gmail.com>
2020-04-04 19:06:04 -0400

    Generate project model; add Ember MIrage
    This commit generates a model for the project, changes the route so that it queries the store for projects, and sets up Ember Mirage to mock a backend that provides projects in the response.

--- client/app/routes/projects.js
+++ client/app/routes/projects.js
@@ -4,70 +4,8 @@ import Route from '@ember/routing/route';
 // will have associated controller, which will handle business logic of sorting project
 // objects into appropriate buckets
 export default class ProjectsRoute extends Route {
-    // where we define the model of this part of the application
-    model() {
-        return [
-            {
-                id: "P2020M7890",
-                name: "Huge New Public Library",
-                primaryApplicant: "Brandyn Friedly",
-                packages: [
-                    {
-                        type: 'pas',
-                        statuscode: 'Saved'
-                    }
-                ]
-            },
-            {
-                id: "P2020M7891",
-                name: "Bagel Factory",
-                primaryApplicant: "Godfrey Yeung",
-                packages: [
-                    {
-                        type: 'pas',
-                        statuscode: 'Package Preparation'
-                    }
-                ]
-            },
-            {
-                id: "P2020M7890",
-                name: "123 Ember Avenue",
-                primaryApplicant: "Matt Gardner",
-                packages: [
-                    {
-                        type: 'pas',
-                        statuscode: 'Submitted'
-                    }
-                ]
-            },
-            {
-                id: "P2020M7891",
-                name: "Arizona Sun Dog Kennel",
-                primaryApplicant: "Taylor McGinnis",
-                packages: [
-                    {
-                        type: 'pas',
-                        statuscode: 'Under Review'
-                    }
-                ]
-            },
-            {
-                id: "P2020M7890",
-                name: "All Things Blue For You Merch",
-                primaryApplicant: "Hannah Kates",
-                packages: [
-                    {
-                        type: 'pas',
-                        statuscode: 'Revision Required'
-                    }
-                ]
-            },
-            {
-                id: "P2020M7891",
-                name: "Pop Up Palm Tree & Goat Farm",
-                primaryApplicant: "Nneka Sobers",
-                packages: []
-            },
-        ]
-    }
+  // where we define the model of this part of the application
+  model() {
+    return this.store.findAll('project');
+  }
 }
