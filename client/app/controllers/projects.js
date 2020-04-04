cec6fadf68c33fe070a7079ec72afe2aaa008c7f
Matt Gardner <wilburnforce@gmail.com>
2020-04-04 19:47:00 -0400

    Enable team ESLint rules; auto-fix linting issues
    This commit adds eslint rule enforcement and auto-fixes a number of eslint issues according the ruleset we decided on in CEQR app project.

--- client/app/controllers/projects.js
+++ client/app/controllers/projects.js
@@ -1,33 +1,28 @@
 import Controller from '@ember/controller';
 
 export default class ProjectsController extends Controller {
-    // projects for applicant to do
-    get applicantProjects () {
-        // handle filtering here, reference in template as this.applicantProjects
-        // todo if statuscode is "Save" or "Package Preparation"
-        return this.model.filter((project) => {
-            return project.packages.some((projectpackage) => {
-                if (projectpackage.statuscode === 'Saved' || projectpackage.statuscode === 'Package Preparation') {
-                    return true
-                    } else { return false }
-            })
+  // projects for applicant to do
+  get applicantProjects () {
+    // handle filtering here, reference in template as this.applicantProjects
+    // todo if statuscode is "Save" or "Package Preparation"
+    return this.model.filter((project) => project.packages.some((projectpackage) => {
+      if (projectpackage.statuscode === 'Saved' || projectpackage.statuscode === 'Package Preparation') {
+        return true;
+      } return false;
+    }));
+  }
 
-        })
-    }
+  // projects for nyc planning to do
+  get planningProjects () {
+    return this.model.filter((project) => project.packages.some((projectpackage) => {
+      if (projectpackage.statuscode === 'Submitted' || projectpackage.statuscode === 'Under Review' || projectpackage.statuscode === 'Revision Required') {
+        return true;
+      } return false;
+    }));
+  }
 
-    // projects for nyc planning to do
-    get planningProjects () {
-        return this.model.filter((project) => {
-            return project.packages.some((projectpackage) => {
-                if (projectpackage.statuscode === 'Submitted' || projectpackage.statuscode === 'Under Review' || projectpackage.statuscode === "Revision Required") {
-                    return true
-                    } else { return false }
-            })
-
-        })
-    }
-
-    get upcomingProjects () {
-        // TODO: write filter for projects with no pas package objects
-    }
+  get upcomingProjects () {
+    // TODO: write filter for projects with no pas package objects
+    return null;
+  }
 }
--- client/app/models/project.js
+++ client/app/models/project.js
@@ -2,6 +2,8 @@ import Model, { attr } from '@ember-data/model';
 
 export default class ProjectModel extends Model {
   @attr name;
+
   @attr primaryApplicant;
+
   @attr packages;
 }
