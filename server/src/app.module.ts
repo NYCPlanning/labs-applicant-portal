fa613e8a5d9bf4dd3f78c84ad31094de86291f20
Matt Gardner <wilburnforce@gmail.com>
2020-04-06 11:19:51 -0400

    Add auth middleware and service features
    This commit exposes a ‘session’ variable

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { AuthMiddleware } from './auth.middleware';
 
 @Module({
   imports: [
@@ -8,4 +9,10 @@ import { AuthModule } from './auth/auth.module';
   ],
   controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes('*');
+  }
+}
