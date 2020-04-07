327f2eac1bd6c5da3f07a1cae1f563100c079edd
Brandyn Friedly <bfriedly@planning.nyc.gov>
2020-04-07 14:53:37 -0400

    Fix landing page sign-in button, redirect to nycid
    This commit is a hot fix to have the big orange sign in button on the landing page to redirect users to nyc id.
    
    It copies the logic used in the sign-in button in an index.js controller (left a todo to make this DRY, but wanted to get this hot fix in for demo).

---
+++ client/app/controllers/index.js
@@ -0,0 +1,16 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import window from 'ember-window-mock';
+import ENV from '../config/environment';
+
+// TODO:  Don't repeat this logic from components/sign-in-button.js, make it reusable!
+export default class IndexController extends Controller {
+  get loginLocation() {
+    return ENV.NYCIDLocation;
+  }
+
+  @action
+  handleLogin() {
+    window.location.href = this.loginLocation;
+  }
+}
