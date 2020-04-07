74d601b39c37ea0cd1543b37c98a0798e34d31e4
Matt Gardner <wilburnforce@gmail.com>
2020-04-06 21:51:00 -0400

    Include cookies with fetch requests
    This commit forces fetch to include credentials. There is an issue with allowing credentials with new version of Ember Data. See:
    
    See https://github.com/emberjs/data/issues/6413

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -3,4 +3,13 @@ import ENV from '../config/environment';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
   host = ENV.host;
+
+  // TODO: find better approach, this is a private method.
+  // see https://github.com/emberjs/data/issues/6413
+  // we must override so it includes the cookie
+  _fetchRequest(options) {
+    options.credentials = 'include';
+
+    return fetch(options.url, options);
+  }
 }
