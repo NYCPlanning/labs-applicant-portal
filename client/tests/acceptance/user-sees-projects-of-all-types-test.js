e5aa4c3ddd10456e5ade6f2d467caf4d7700bc7b
godfreyyeung <godfreyyeung@gmail.com>
2020-04-20 15:09:17 -0400

    87 Revise text for 'no applicant projects' and 'no active dcp projects whatsoever' scenarios
    This commit updates the text shown when there are no applicant (To Do) projects, and also
    the text for when there are no Active, Applicant-Only and General Public projects whatsoever
    associated with a contact.
    
    the applicant (To Do) projects are a subset of the Active, Applicant-Only and General Public
    projects (which are any projects returned from the /projects API endpoint).

--- client/tests/acceptance/user-sees-projects-of-all-types-test.js
+++ client/tests/acceptance/user-sees-projects-of-all-types-test.js
@@ -27,9 +27,18 @@ module('Acceptance | user sees projects of all types', function(hooks) {
     assert.equal(findAll('[data-test-view-pas]').length, 5);
   });
 
-  test('Page should display non-assigned message if no projects', async function(assert) {
+  test('Page should display "No response required" message if no applicant projects', async function(assert) {
+    this.server.createList('project', 1, 'planning');
+
     await visit('/projects');
-    assert.ok(find('[data-test-not-assigned]'));
+
+    assert.ok(find('[data-test-no-response-required]'));
+    assert.notOk(find('[data-test-not-assigned-any-active-project]'));
+  });
+
+  test('Page should display "Not assigned any active projects" message if no active projects whatsoever', async function(assert) {
+    await visit('/projects');
+    assert.ok(find('[data-test-not-assigned-any-active-project]'));
   });
 
   test('Projects are listed alphabetically', async function (assert) {
