ef67c967fcaca1aba93e2c2f998665f5969884a5
godfreyyeung <godfreyyeung@gmail.com>
2020-04-23 19:03:04 -0400

    23 list documents already on the package.
    The Document properties are derived from the metadata about
    a package's documents  returned by a CRM Web API call hitting
    the GetFileByServerRelativePath(package_id)/Files endpoint

---
+++ client/app/components/packages/attachments.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+
+/**
+  * This component creates a new File Manager specifically for
+  * the passed in package The File Mananger is among one of many
+  * on the the applicantion File Manager service.
+  * 
+  * This cm
+  * @param      {Package Model}  package
+  */
+export default class PackagesAttachmentsComponent extends Component {
+}
--- client/tests/integration/components/packages/attachments-test.js
+++ client/tests/integration/components/packages/attachments-test.js
@@ -1,14 +1,36 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | packages/attachments', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it displays a list attachments already uploaded to the package', async function(assert) {
-    await render(hbs`<Packages::Attachments />`);
+    this.package = {
+      documents: [
+        {
+          name: 'PAS Form.pdf',
+          // TODO: Format that this is the final serialized
+          // format of the "timeCreated" property 
+          timeCreated: '2020-04-23T22:35:30Z',
+          id: '59fbf112-71a5-4af5-b20a-a746g08c4c6p',
+        },
+        {
+          name: 'Action Changes.excel',
+          timeCreated: '2020-02-21T22:25:10Z',
+          id: 'f0f2f3a3-3936-499b-8f37-a9827a1c14f2',
+        },
+      ]
+    };
 
-    await this.pauseTest();
+    await render(hbs`<
+      Packages::Attachments
+      @package={{this.package}}
+     />`);
+
+
+     assert.equal(find('[data-test-document-name="0"]').textContent.trim(), 'PAS Form.pdf');
+     assert.equal(find('[data-test-document-name="1"]').textContent.trim(), 'Action Changes.excel');
   });
 });
