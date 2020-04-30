71bf8d9f8c4303a3369eda3ba2c1e0669deaedb8
Brandyn Friedly <bfriedly@planning.nyc.gov>
2020-04-30 14:02:14 -0400

    Update team editor component

--- client/app/components/packages/applicant-team-editor.js
+++ client/app/components/packages/applicant-team-editor.js
@@ -1,16 +1,28 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ApplicantTeamEditorComponent extends Component {
+  @service
+  store;
+
+  // allow a user to add a new applicant fieldset
   @action
-  addApplicant(title) {
-    console.log(this.args.applicants);
-    this.args.applicants.pushObject({ title });
+  addApplicant(role) {
+    // create a new applicant record in store
+    const newApplicant = this.store.createRecord('applicant', {role});
+
+    // add the record to the array managing the editor's UI
+    this.args.applicants.pushObject(newApplicant);
   }
 
-  // TODO: figure out how to delete this object
+  // allow a user to remove an applicant fieldset
   @action removeApplicant(applicant) {
-    console.log(`REMOVE APPLICANT:  ${applicant}`) // logs "REMOVE APPLICANT:  model:applicant(:id)"
+    // remove the applicant from the ember store
+    this.store.deleteRecord(applicant);
+
+    // remove the DOM node by removing the object from the array passed to the editor component
+    this.args.applicants.removeObject(applicant);
   }
 }
--- client/app/models/applicant.js
+++ client/app/models/applicant.js
@@ -2,26 +2,37 @@ import Model, { attr } from '@ember-data/model';
 
 export default class ApplicantModel extends Model {
   // indicates which table to send to crm ("Applicant" or "Applicant Representative")
-  @attr('string') role;
+  @attr('string')
+  role;
 
   // doesn't exist on "Applicant Representative Information" CRM table
-  @attr('string') applicantType;
+  @attr('string')
+  applicantType;
 
-  @attr('string') firstName;
+  @attr('string') 
+  firstName;
 
-  @attr('string') lastName;
+  @attr('string')
+  lastName;
 
-  @attr('string') organization;
+  @attr('string')
+  organization;
 
-  @attr('string') emailAddress;
+  @attr('string')
+  emailAddress;
 
-  @attr('string') address;
+  @attr('string')
+  address;
 
-  @attr('string') city;
+  @attr('string')
+  city;
 
-  @attr('string') state;
+  @attr('string')
+  state;
 
-  @attr('string') zip;
+  @attr('string')
+  zip;
 
-  @attr('string') phone;
+  @attr('string')
+  phone;
 }
