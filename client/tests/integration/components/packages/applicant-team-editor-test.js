5c7d0acc730df4657ecafec679b99df2395231a0
Brandyn Friedly <bfriedly@planning.nyc.gov>
2020-04-30 14:01:25 -0400

    Scaffold tests
    Scaffolds the integrations tests that need to be written.

--- client/tests/integration/components/packages/applicant-team-editor-test.js
+++ client/tests/integration/components/packages/applicant-team-editor-test.js
@@ -9,16 +9,46 @@ module('Integration | Component | packages/applicant-team-editor', function(hook
   setupRenderingTest(hooks);
   setupMirage(hooks);
 
-  test('it renders', async function(assert) {
-    this.testApplicants = this.server.createList('applicant', 2, 'organizationApplicant');
+  test('user has a blank fieldset when there are no existing applicants', async function(assert) {
+    const emptyApplicantsArray = [];
+    await render(hbs`
+      <Packages::ApplicantTeamEditor
+        @applicants={{this.emptyApplicantsArray}}
+      />
+    `);
+  });
 
+  test('user can see existing applicants', async function(assert) {
+    // 3 scenarios/permutations of kinds of applicants
+    this.server.create('applicant', 'organizationApplicant');
+    this.server.create('applicant', 'individualApplicant');
+    this.server.create('applicant', 'applicantTeamMember');
+
+    const applicant1 = await this.owner.lookup('service:store').findRecord('applicant', 1);
+    const applicant2 = await this.owner.lookup('service:store').findRecord('applicant', 2);
+    const applicant3 = await this.owner.lookup('service:store').findRecord('applicant', 3);
+  
+    this.applicantsArray = [applicant1, applicant2, applicant3];
+  
     await render(hbs`
       {{log "Rendering Packages::ApplicantTeamEditor..."}}
       <Packages::ApplicantTeamEditor 
-        @applicants={{this.testApplicants}}
+        @applicants={{this.applicantsArray}}
       />
-    `)
+    `);
 
     await this.pauseTest();
   });
+
+  test('user can add new applicants', async function(assert) {
+
+  });
+
+  test('user can add new applicant team members', async function(assert) {
+
+  });
+
+  test('user can remove applicants', async function(assert) {
+    
+  })
 });
