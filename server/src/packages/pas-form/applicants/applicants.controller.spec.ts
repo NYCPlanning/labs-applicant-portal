9ccfadec6ba39827f5851791319d088515fc07c6
Matt Gardner <wilburnforce@gmail.com>
2020-05-01 11:12:13 -0400

    Fix unit tests

--- server/src/packages/pas-form/applicants/applicants.controller.spec.ts
+++ server/src/packages/pas-form/applicants/applicants.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApplicantsController } from './applicants.controller';
+import { CrmModule } from '../../../crm/crm.module';
 
 describe('Applicants Controller', () => {
   let controller: ApplicantsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CrmModule],
       controllers: [ApplicantsController],
     }).compile();
 
--- server/src/packages/pas-form/pas-form.controller.spec.ts
+++ server/src/packages/pas-form/pas-form.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PasFormController } from './pas-form.controller';
+import { CrmModule } from '../../crm/crm.module';
 
 describe('PasForm Controller', () => {
   let controller: PasFormController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CrmModule],
       controllers: [PasFormController],
     }).compile();
 
