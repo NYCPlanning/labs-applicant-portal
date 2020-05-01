500d1384a38617fbbcff4c959a09ae1420ddb3f0
Andy Cochran <acochran@planning.nyc.gov>
2020-05-01 09:45:10 -0400

    fix PasForm integration tests attr names

--- client/tests/integration/components/pas-form-test.js
+++ client/tests/integration/components/pas-form-test.js
@@ -59,9 +59,9 @@ module('Integration | Component | pas-form', function(hooks) {
     this.package = await this.owner.lookup('service:store').findRecord('package', projectPackage.id, { include: 'pas-form' });
 
     await render(hbs`<PasForm @package={{this.package}} />`);
-    assert.dom('[data-test-dcpproposeddevelopmentsiteinfoother]').doesNotExist();
-    await click('[data-test-dcpproposeddevelopmentsiteotherexplanation]');
-    assert.dom('[data-test-dcpproposeddevelopmentsiteinfoother]').exists();
+    assert.dom('[data-test-dcpproposeddevelopmentsiteotherexplanation]').doesNotExist();
+    await click('[data-test-dcpproposeddevelopmentsiteinfoother]');
+    assert.dom('[data-test-dcpproposeddevelopmentsiteotherexplanation]').exists();
   });
 
   test('MIH sub Q shows conditionally', async function(assert) {
@@ -81,14 +81,14 @@ module('Integration | Component | pas-form', function(hooks) {
     this.package = await this.owner.lookup('service:store').findRecord('package', projectPackage.id, { include: 'pas-form' });
 
     await render(hbs`<PasForm @package={{this.package}} />`);
-    assert.dom('[data-test-dcphousingunittypecity]').doesNotExist();
-    assert.dom('[data-test-dcphousingunittypestate]').doesNotExist();
-    assert.dom('[data-test-dcphousingunittypefederal]').doesNotExist();
-    assert.dom('[data-test-dcphousingunittypeother]').doesNotExist();
+    assert.dom('[data-test-dcphousingunittype-city]').doesNotExist();
+    assert.dom('[data-test-dcphousingunittype-state]').doesNotExist();
+    assert.dom('[data-test-dcphousingunittype-federal]').doesNotExist();
+    assert.dom('[data-test-dcphousingunittype-other]').doesNotExist();
     await click('[data-test-dcpdiscressionaryfundingforffordablehousing-yes]');
-    assert.dom('[data-test-dcphousingunittypecity]').exists();
-    assert.dom('[data-test-dcphousingunittypestate]').exists();
-    assert.dom('[data-test-dcphousingunittypefederal]').exists();
-    assert.dom('[data-test-dcphousingunittypeother]').exists();
+    assert.dom('[data-test-dcphousingunittype-city]').exists();
+    assert.dom('[data-test-dcphousingunittype-state]').exists();
+    assert.dom('[data-test-dcphousingunittype-federal]').exists();
+    assert.dom('[data-test-dcphousingunittype-other]').exists();
   });
 });
