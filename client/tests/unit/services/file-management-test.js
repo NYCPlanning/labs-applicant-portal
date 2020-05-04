a5b91ae8ca6e9cc1f1aafa902676fb3155d2e49f
Matt Gardner <wilburnforce@gmail.com>
2020-05-04 10:10:59 -0400

    Add test for file management service

--- client/tests/unit/services/file-management-test.js
+++ client/tests/unit/services/file-management-test.js
@@ -9,4 +9,15 @@ module('Unit | Service | fileManagement', function(hooks) {
     const service = this.owner.lookup('service:file-management');
     assert.ok(service);
   });
+
+  test('it returns already-registered file manager', function(assert) {
+    const service = this.owner.lookup('service:file-management');
+
+    service.findOrCreate(1, []);
+    service.findOrCreate(1, []);
+
+    // This is not a great test. But I'm not sure there's a way to prove this class
+    // doesn't duplicate things/recreate things.
+    assert.ok(service);
+  });
 });
