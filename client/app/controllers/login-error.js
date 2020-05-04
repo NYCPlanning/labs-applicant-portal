8400411919a3e77ad099a5219534f463862da05e
Matt Gardner <wilburnforce@gmail.com>
2020-05-04 11:34:32 -0400

    Use JSON:API compatible error response
    InvalidError-wrapped internal errors are compatible with network-specific errors.

--- client/app/controllers/login-error.js
+++ client/app/controllers/login-error.js
@@ -6,10 +6,6 @@ export default class LoginErrorController extends Controller {
     *                     about Contact not existing in CRM.
     */
   get contactNotAssigned() {
-    if (this.model.errors) {
-      return this.model.errors.some((error) => error.response.code === 'NO_CONTACT_FOUND');
-    }
-
-    return [];
+    return this.model.errors.some((error) => error.response.code === 'NO_CONTACT_FOUND');
   }
 }
