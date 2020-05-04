a306e8309f5e32ff52e768fc3b32b15add215860
Andy Cochran <acochran@planning.nyc.gov>
2020-05-04 16:18:03 -0400

    add confirmation modal to PAS form

--- client/app/components/packages/pas-form/edit.js
+++ client/app/components/packages/pas-form/edit.js
@@ -3,10 +3,17 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class PasFormComponent extends Component {
+  @tracked modalIsOpen = false;
+
   @tracked package;
 
   @action
   updateAttr(obj, attr, newVal) {
     obj[attr] = newVal;
+  }
+
+  @action
+  toggleModal() {
+    this.modalIsOpen = !this.modalIsOpen;
   }
 }
