b5198317175da7b7dc7bf62408299e5b905a32b5
brandyn <bfriedly@planning.nyc.gov>
2020-05-06 17:50:36 -0400

    Add (very basic) PAS Form data to view-only page (#189)
    * Add PAS Form data to view-only page
    
    This commit adds in the pas form data in a very basic way to just get it represented on the page.
    
    * Prettier format hbs templte

--- client/app/routes/packages/show.js
+++ client/app/routes/packages/show.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default class PackagesShowRoute extends Route {
+  model(params) {
+    return this.store.findRecord('package', params.id, {
+      reload: true,
+      include: 'pasForm',
+    });
+  }
 }
