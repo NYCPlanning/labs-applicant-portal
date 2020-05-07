4427f4ef778a18fec51e6a311248b4f01403671f
Andy Cochran <acochran@planning.nyc.gov>
2020-05-07 13:18:49 -0400

    display user's email address in header

--- client/app/controllers/application.js
+++ client/app/controllers/application.js
@@ -5,6 +5,8 @@ import { inject as service } from '@ember/service';
 export default class ApplicationController extends Controller {
   @service session;
 
+  userEmail = this.session.data.authenticated.emailaddress1;
+
   @action
   doLogout() {
     this.transitionToRoute('logout');
