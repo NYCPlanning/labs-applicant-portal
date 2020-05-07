57eaec64ac5aaf4cd95fe84ff07b43a21b44ca71
Andy Cochran <acochran@planning.nyc.gov>
2020-05-07 15:57:04 -0400

    access this.session.data.authenticated.emailaddress1 directly

--- client/app/controllers/application.js
+++ client/app/controllers/application.js
@@ -5,8 +5,6 @@ import { inject as service } from '@ember/service';
 export default class ApplicationController extends Controller {
   @service session;
 
-  userEmail = this.session.data.authenticated.emailaddress1;
-
   @action
   doLogout() {
     this.transitionToRoute('logout');
