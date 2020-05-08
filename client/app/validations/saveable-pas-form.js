61c2341855529ee333144cfa5a2e9699e7812726
Matt Gardner <wilburnforce@gmail.com>
2020-05-08 17:19:28 -0400

    Use multiple changesets with a merge

--- client/app/validations/saveable-pas-form.js
+++ client/app/validations/saveable-pas-form.js
@@ -6,49 +6,50 @@ export default {
   dcpRevisedprojectname: [
     validateLength({
       max: 50,
-      message: 'Name is too long (max 50 characters)',
+      min: 4,
+      message: 'Name must be between {min} and {max} characters',
     }),
   ],
 
   dcpDescriptionofprojectareageography: [
     validateLength({
       max: 2000,
-      message: 'Description is too long (max 2000 characters)',
+      message: 'Description is too long (max {max} characters)',
     }),
   ],
 
   dcpUrbanareaname: [
     validateLength({
       max: 250,
-      message: 'Name is too long (max 250 characters)',
+      message: 'Name is too long (max {max} characters)',
     }),
   ],
 
   dcpPleaseexplaintypeiienvreview: [
     validateLength({
       max: 200,
-      message: 'Text is too long (max 200 characters)',
+      message: 'Text is too long (max {max} characters)',
     }),
   ],
 
   dcpProjectareaindutrialzonename: [
     validateLength({
       max: 250,
-      message: 'Name is too long (max 250 characters)',
+      message: 'Name is too long (max {max} characters)',
     }),
   ],
 
   dcpProjectarealandmarkname: [
     validateLength({
       max: 250,
-      message: 'Name is too long (max 250 characters)',
+      message: 'Name is too long (max {max} characters)',
     }),
   ],
 
   dcpCityregisterfilenumber: [
     validateLength({
       max: 25,
-      message: 'File Number is too long (max 25 characters)',
+      message: 'File Number is too long (max {max} characters)',
     }),
   ],
 
@@ -62,21 +63,21 @@ export default {
   dcpProjectdescriptionproposeddevelopment: [
     validateLength({
       max: 3000,
-      message: 'Text is too long (max 3000 characters)',
+      message: 'Text is too long (max {max} characters)',
     }),
   ],
 
   dcpProjectdescriptionbackground: [
     validateLength({
       max: 2000,
-      message: 'Text is too long (max 2000 characters)',
+      message: 'Text is too long (max {max} characters)',
     }),
   ],
 
   dcpProjectdescriptionproposedactions: [
     validateLength({
       max: 2000,
-      message: 'Text is too long (max 2000 characters)',
+      message: 'Text is too long (max {max} characters)',
     }),
   ],
 
