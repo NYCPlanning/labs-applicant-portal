8154fc7fcd75350efc71e821feed5040524e0ca3
trbmcginnis <taylormcgin@gmail.com>
2020-05-12 15:39:00 -0500

    168: add styling, functionality, and tests for pas-form visual error handling

--- client/app/components/packages/pas-form/edit.js
+++ client/app/components/packages/pas-form/edit.js
@@ -78,18 +78,30 @@ export default class PasFormComponent extends Component {
   }
 
   @task(function* () {
-    yield this.saveableChanges.execute();
-    yield this.saveableChanges.rollback();
+    try {
+      // in case there are errors on saving/patching,
+      // this should occur BEFORE the changeset executions & rollbacks
+      yield this.args.onSave();
 
-    yield this.args.onSave();
+      yield this.saveableChanges.execute();
+      yield this.saveableChanges.rollback();
+    } catch (adapterError) {
+      console.log('Save error:', adapterError);
+    }
   })
   save;
 
   @task(function* () {
-    yield this.submittableChanges.execute();
-    yield this.submittableChanges.rollback();
+    try {
+      // in case there are errors on saving/patching,
+      // this should occur BEFORE the changeset executions & rollbacks
+      yield this.args.onSubmit();
 
-    yield this.args.onSubmit();
+      yield this.submittableChanges.execute();
+      yield this.submittableChanges.rollback();
+    } catch (adapterError) {
+      console.log('Submit error:', adapterError);
+    }
   })
   submit;
 
