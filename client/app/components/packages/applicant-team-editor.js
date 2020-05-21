b2e1b68b20fdbe5a54321e222490ca0d69235fe0
Brandyn Friedly <bfriedly@planning.nyc.gov>
2020-05-21 12:16:43 -0400

    Update add and remove applicant state management
    Previously, if the applicants array was empty we were adding a blank applicant record so we could render an empty applicant fieldset for the user.   This commit removes that to simplify UX and DX.
    
    When a user clicked “Remove Applicant” button, we were using model.destroyRecord(), which would immediately send a DELETE request to the api server.
    
    In this commit, we use model.deleteRecord() instead, which sets model.isDeleted = true.  This queues the record for deletion when the user clicks “Save” (which ultimately will trigger model.save(), where Ember Data makes the api reqeust to DELETE.

--- client/app/components/packages/applicant-team-editor.js
+++ client/app/components/packages/applicant-team-editor.js
@@ -19,18 +19,6 @@ export default class ApplicantTeamEditorComponent extends Component {
   // allow a user to remove an applicant fieldset
   @action
   removeApplicant(applicant) {
-    // remove the applicant from the ember store
-    // this.store.deleteRecord(applicant);
-    applicant.destroyRecord();
-
-    // remove the DOM node by removing the object from the array passed to the editor component
-    this.args.applicants.removeObject(applicant);
-  }
-
-  @action
-  addApplicantIfEmpty() {
-    if (!this.args.applicants.length) {
-      this.addApplicant();
-    }
+      applicant.deleteRecord();
   }
 }
