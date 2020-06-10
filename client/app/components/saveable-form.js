9e369ce98f99ef4878427ce5c97a9fe7f6ea30cd
Matt Gardner <wilburnforce@gmail.com>
2020-06-10 12:09:11 -0400

    Rename and comment getters, args, for save-button

--- client/app/components/saveable-form.js
+++ client/app/components/saveable-form.js
@@ -2,6 +2,8 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class SaveableFormComponent extends Component {
+  // TODO: Understand why this particular sequencing is necessary.
+  // Can the callback happen afterwards?
   async saveChangeset(changeset, callback) {
     try {
       await changeset.execute();
