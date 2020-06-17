b73e18903b5ba71debc2f4b921f8c9359e887a7a
allthesignals <wilburnforce@gmail.com>
2020-06-16 20:55:48 -0400

    Introduce FormValidator component

---
+++ client/tests/integration/components/form-validator-test.js
@@ -0,0 +1,25 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | form-validator', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    this.dummyModel = {};
+    this.validator = {};
+
+    // Template block usage:
+    await render(hbs`
+      <FormValidator
+        @model={{this.dummyModel}}
+        @validator={{this.validator}} as |changeset myValidator|
+      >
+        {{myValidator.isValid}}, {{myValidator.isDirty}}
+      </FormValidator>
+    `);
+
+    assert.dom().hasText('true, false');
+  });
+});
