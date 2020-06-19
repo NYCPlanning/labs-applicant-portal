8e1ef60685489c16a6fe0c6cd2efc9cbae942b52
godfreyyeung <godfreyyeung@gmail.com>
2020-06-19 15:40:31 -0400

    Remove unecessary top-lvl txt and radio components

--- client/app/components/radio-button.js
+++
@@ -1,6 +0,0 @@
-import RadioButtonComponent from 'ember-radio-button/components/radio-button';
-import { guidFor } from '@ember/object/internals';
-
-export default class GroupIdentifier extends RadioButtonComponent {
-  radioId = `radio-${guidFor(this)}`;
-}
--- client/app/components/text-area.js
+++
@@ -1,5 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class TextAreaComponent extends Component {
-  maxlength = this.args.maxlength || '2000';
-}
