4fe57f07871982bb5b21abe39954aa00c3b10aab
Andy Cochran <andycochran@users.noreply.github.com>
2020-06-22 17:56:46 -0400

    Ui::Question component (#432)
    * refactor Ui::Fieldset to Ui::Question to work for labels too
    
    * use Question component component and sections
    
    * note TODO in land-use-action.hbs
    
    * change the validation message classes to follow our style guide
    
    * nix some unnecessary CSS

--- client/tests/integration/components/ui/question-test.js
+++ client/tests/integration/components/ui/question-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | ul/fieldset', function(hooks) {
+module('Integration | Component | ul/question', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Ui::Fieldset />`);
+    await render(hbs`<Ui::Question />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <Ui::Fieldset>
+      <Ui::Question>
         template block text
-      </Ui::Fieldset>
+      </Ui::Question>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
