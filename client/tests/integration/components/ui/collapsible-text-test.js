f8b73440e6d66387c0bb7eb81febb602869f58f1
trbmcginnis <taylormcgin@gmail.com>
2020-07-01 15:00:16 -0400

    232: add new component collapsible-text

---
+++ client/tests/integration/components/ui/collapsible-text-test.js
@@ -0,0 +1,24 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, click } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | ui/collapsible-text', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('clicking on additional text link renders correct text', async function(assert) {
+    await render(hbs`
+      <Ui::CollapsibleText @buttonText="My Additional Text Title">
+        Here is my additional text.
+      </Ui::CollapsibleText>
+    `);
+
+    assert.dom('[data-test-title="My Additional Text Title"]').hasText('My Additional Text Title');
+    assert.dom('[data-test-additional-text]').doesNotExist();
+
+    await click('[data-test-title="My Additional Text Title"]');
+
+    assert.dom('[data-test-title="My Additional Text Title"]').hasText('My Additional Text Title');
+    assert.dom('[data-test-additional-text]').hasText('Here is my additional text.');
+  });
+});
