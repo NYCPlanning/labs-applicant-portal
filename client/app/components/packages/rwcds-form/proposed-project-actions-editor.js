f1d1882dc23768332065bdeea28656ef55b1d8d8
trbmcginnis <taylormcgin@gmail.com>
2020-07-02 11:41:32 -0400

    223: add zrsectiontitle question to proposed actions component

--- client/app/components/packages/rwcds-form/proposed-project-actions-editor.js
+++ client/app/components/packages/rwcds-form/proposed-project-actions-editor.js
@@ -1,34 +1,40 @@
 import Component from '@glimmer/component';
 
 export default class ProposedActionsComponent extends Component {
-  actionsWithSectionNumber = [
+  actionsWithSectionNumberAndSectionTitle = [
     'ZS', 'ZA', 'ZC', 'SD', 'SA', 'SC', 'RS', 'RA', 'RC',
   ];
 
   actionsWithModifiedZrSectionNumberQuestion = [
-    'ZS', 'ZA', 'SD', 'SA', 'RS', 'RA',
+    'ZA', 'ZS', 'SD', 'SA', 'RA', 'RS',
   ];
 
   get zrAppendixF() {
-    const zrType = this.args.form.saveableChanges.dcpZoningresolutiontype;
-    const zrSectionNumber = this.args.form.saveableChanges.dcpZrsectionnumber;
+    const zrType = this.args.zrForm.saveableChanges.dcpZoningresolutiontype;
+    const zrSectionNumber = this.args.zrForm.saveableChanges.dcpZrsectionnumber;
 
     return zrType === 'ZR' && zrSectionNumber === 'Appendix F';
   }
 
   get zrSectionNumber() {
-    const zrType = this.args.form.saveableChanges.dcpZoningresolutiontype;
-    const zrSectionNumber = this.args.form.saveableChanges.dcpZrsectionnumber;
+    const zrType = this.args.zrForm.saveableChanges.dcpZoningresolutiontype;
+    const zrSectionNumber = this.args.zrForm.saveableChanges.dcpZrsectionnumber;
 
-    const zrTypeInArray = this.actionsWithSectionNumber.includes(zrType);
+    const zrTypeInArray = this.actionsWithSectionNumberAndSectionTitle.includes(zrType);
     if (zrTypeInArray || this.zrAppendixF) {
       return zrSectionNumber;
     } return null;
   }
 
   get hasModifiedZrSectionNumberQuestion() {
-    const zrType = this.args.form.saveableChanges.dcpZoningresolutiontype;
+    const zrType = this.args.zrForm.saveableChanges.dcpZoningresolutiontype;
     const zrTypeInArray = this.actionsWithModifiedZrSectionNumberQuestion.includes(zrType);
     return zrTypeInArray;
+  }
+
+  get hasZrSectionTitleQuestion() {
+    const zrType = this.args.zrForm.saveableChanges.dcpZoningresolutiontype;
+    const zrTypeInArray = this.actionsWithSectionNumberAndSectionTitle.includes(zrType);
+    return zrTypeInArray || this.zrAppendixF;
   }
 }
