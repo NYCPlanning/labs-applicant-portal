61b623d0590f40fc3028eb4abcf2824187941e75
Andy Cochran <acochran@planning.nyc.gov>
2020-07-13 13:41:37 -0400

    revised Ui::Question API
    improve the CollapsibleText and Question component styles
    
    revise PAS, RWCDS forms using refactored Ui::Question

--- client/tests/integration/components/ui/label-test.js
+++ client/tests/integration/components/ui/label-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | ui/fieldset-legend', function(hooks) {
+module('Integration | Component | ui/label', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Ui::FieldsetLegend />`);
+    await render(hbs`<Ui::Label />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <Ui::FieldsetLegend>
+      <Ui::Label>
         template block text
-      </Ui::FieldsetLegend>
+      </Ui::Label>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
