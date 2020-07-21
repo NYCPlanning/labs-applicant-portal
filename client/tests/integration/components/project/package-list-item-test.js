7913ac181be19292f30ec08fe09713c53a85c4fe
trbmcginnis <taylormcgin@gmail.com>
2020-07-21 12:17:34 -0400

    378: rename package-list component to package-list-item

--- client/tests/integration/components/project/package-list-item-test.js
+++ client/tests/integration/components/project/package-list-item-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | project/package-list', function(hooks) {
+module('Integration | Component | project/package-list-item', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Project::PackageList />`);
+    await render(hbs`<Project::PackageListItem />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <Project::PackageList>
+      <Project::PackageListItem>
         template block text
-      </Project::PackageList>
+      </Project::PackageListItem>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
