84454f48d45aa2b5e62080c85645bbbb247cdfda
Andy Cochran <acochran@planning.nyc.gov>
2020-07-23 12:13:18 -0400

    nix skipped/crufty PlanningProjectCard test

--- client/tests/integration/components/planning-project-card-test.js
+++
@@ -1,8 +0,0 @@
-import { module, skip } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-
-module('Integration | Component | planning-project-card', function(hooks) {
-  setupRenderingTest(hooks);
-
-  skip('it renders');
-});
