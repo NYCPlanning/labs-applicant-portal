6ef055fafbd92c4f838875c334a7ca57dcd4bf51
allthesignals <wilburnforce@gmail.com>
2020-08-04 13:41:37 -0400

    Extract user badge to new component; add edit link
    Includes a base64 conversion for the NYC.ID target param

--- client/app/controllers/application.js
+++ client/app/controllers/application.js
@@ -1,12 +1,6 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
   @service session;
-
-  @action
-  doLogout() {
-    this.transitionToRoute('logout');
-  }
 }
