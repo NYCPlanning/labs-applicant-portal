d5783761f5053ab271d65372cd791cf3f5007730
trbmcginnis <taylormcgin@gmail.com>
2020-09-04 17:34:14 -0400

    12257: create environmental review component on landuse-form

---
+++ client/app/optionsets/landuse-form.js
@@ -0,0 +1,14 @@
+export const CEQR_TYPE = {
+  TYPE_I: {
+    code: 717170000,
+    label: 'Type I',
+  },
+  TYPE_II: {
+    code: 717170001,
+    label: 'Type II',
+  },
+  UNLISTED: {
+    code: 717170002,
+    label: 'Unlisted',
+  },
+};
