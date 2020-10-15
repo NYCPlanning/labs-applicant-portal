85891cb2101f8b9c01b8fab8eec795414df579e0
dhochbaum-dcp <dhochbaum@planning.nyc.gov>
2020-10-15 14:44:19 -0400

    Made first changes to model

--- client/app/models/invoice.js
+++ client/app/models/invoice.js
@@ -1,4 +1,4 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr } from "@ember-data/model";
 
 export default class InvoiceModel extends Model {
   @attr()
@@ -6,4 +6,45 @@ export default class InvoiceModel extends Model {
 
   @attr()
   dcpName;
+
+  @attr()
+  dcpSubtotal;
+
+  @attr()
+  dcpTwoHundredPercentRule;
+
+  @attr()
+  dcpProjectFees;
+
+  @attr()
+  dcpSupplementalFee;
+
+  @attr()
+  dcpGrandTotal;
 }
+
+// dcpName: 1234567893,
+// dcpProjectname: "305-315 Tillbert Street (CEQR)",
+// dcpInvoicedate: "04/18/20",
+// dcpInvoiceApplications: [
+//   {
+//     dcpApplicationNumber: "210081RCR",
+//     dcpAction: "RC",
+//     dcpFee: "$160.00",
+//   },
+//   {
+//     dcpApplicationNumber: "[ULURP Number]",
+//     dcpAction: "[Code]",
+//     dcpFee: "[amount]",
+//   },
+//   {
+//     dcpApplicationNumber: "[ULURP Number]",
+//     dcpAction: "[Code]",
+//     dcpFee: "[amount]",
+//   },
+// ],
+// dcpSubtotal: "$160.00",
+// dcpTwoHundredPercentRule: "$320.00",
+// dcpProjectFees: "$160.00",
+// dcpSupplementalFee: "$0.00",
+// dcpGrandTotal: "$160.00",
