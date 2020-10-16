b5f29fa346ea918b9bcb5747b0732c5e30be39d0
allthesignals <wilburnforce@gmail.com>
2020-10-16 17:44:20 -0400

    Create a new package section; Hit link generation endpoint

--- client/ember-cli-build.js
+++ client/ember-cli-build.js
@@ -16,7 +16,7 @@ module.exports = function(defaults) {
       sourcemap: false,
     },
     'ember-composable-helpers': {
-      only: ['toggle', 'map-by'],
+      only: ['toggle', 'map-by', 'reduce'],
     },
   });
 
