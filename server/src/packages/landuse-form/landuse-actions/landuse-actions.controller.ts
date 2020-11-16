66b05cec7580f88541ee2b91117ff66bf0c1b252
godfreyyeung <godfreyyeung@gmail.com>
2020-11-16 02:19:30 -0800

    13309 Add ability to clear LU Zoning Resolution dropdown
    The dropdown is part of the Proposed Action Editor

--- server/src/packages/landuse-form/landuse-actions/landuse-actions.controller.ts
+++ server/src/packages/landuse-form/landuse-actions/landuse-actions.controller.ts
@@ -28,15 +28,30 @@ export class LanduseActionsController {
 
   @Patch('/:id')
   async update(@Body() body, @Param('id') id) {
-    const allowedAttrs = pick(body, LANDUSE_ACTION_ATTRS);
+    let allowedAttrs = pick(body, LANDUSE_ACTION_ATTRS);
     const dateOfPreviousApproval = this.pickDate(body.dcp_dateofpreviousapproval);
     const lapseDateOfPreviousApproval = this.pickDate(body.dcp_lapsedateofpreviousapproval);
     const recordationDate = this.pickDate(body.dcp_recordationdate);
 
+    // "chosen_zoning_resolution_id" is the id to a related resource,
+    // but, unlike other attributes, it may not always be included
+    // in the request body.
+    // If a request does not include all of the relationships
+    // for a resource, the server MUST interpret the missing relationships
+    // as if they were included with their current values. It MUST NOT
+    // interpret them as null or empty values.
+    // Therefore, we explicitly check for a null value
+    // (which would imply a key for chosen_zoning_resolution_id),
+    // to avoid assuming that falsiness indicates a disassociation.
+    if (body.chosen_zoning_resolution_id) {
+      allowedAttrs['dcp_zoningresolutionsectionactionispursuantto@odata.bind'] = `/dcp_zoningresolutions(${body.chosen_zoning_resolution_id})`;
+    } else if (body.chosen_zoning_resolution_id === null) {
+      await this.crmService.disassociateHasOne('dcp_zoningresolutionsectionactionispursuantto', 'dcp_landuseactions', id);
+    }
+
     await this.crmService.update('dcp_landuseactions', id, {
       ...allowedAttrs,
 
-      ...(body.chosen_zoning_resolution_id ? { 'dcp_zoningresolutionsectionactionispursuantto@odata.bind': `/dcp_zoningresolutions(${body.chosen_zoning_resolution_id})` } : {}),
       dcp_dateofpreviousapproval: dateOfPreviousApproval,
       dcp_lapsedateofpreviousapproval: lapseDateOfPreviousApproval,
       dcp_recordationdate: recordationDate,
