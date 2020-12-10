b212290b6d8146bbcf56a192d48c31e91ab6201a
trbmcginnis <taylormcgin@gmail.com>
2020-12-10 12:10:25 -0600

    13515: update subtotal value on ceqr invoice questionnaire based on square footage value

---
+++ client/tests/integration/helpers/square-footage-to-subtotal-test.js
@@ -0,0 +1,16 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | square-footage-to-subtotal-lookup', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    this.set('inputValue', 717170001);
+
+    await render(hbs`{{square-footage-to-subtotal inputValue}}`);
+
+    assert.equal(this.element.textContent.trim(), 460);
+  });
+});
