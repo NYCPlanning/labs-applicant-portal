d31651c77b6f82111c9e7d82945f97fce553e16a
trbmcginnis <taylormcgin@gmail.com>
2021-02-02 15:09:35 -0600

    825: disallow users to add the same bbl twice

--- client/app/components/packages/project-geography.js
+++ client/app/components/packages/project-geography.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 import { bblBreakup } from '../../helpers/bbl-breakup';
 import { BOROUGHS } from '../../optionsets/bbl';
 
@@ -19,8 +20,12 @@ export default class ProjectGeographyComponent extends Component {
   @service
   store;
 
+  @tracked bblAlreadyCreated = false;
+
+  @tracked duplicateBblNumber = '';
+
   @action
-  selectSearchResult(labsSearchResult) {
+  async selectSearchResult(labsSearchResult) {
     // `labsSearchResult` is object with `label` (address) and `bbl` attributes.
     // labsSearchResult.bbl comes back as a number,
     // whereas dcpBblnumber in CRM is stored as a string.
@@ -28,26 +33,36 @@ export default class ProjectGeographyComponent extends Component {
 
     const { borough, block, lot } = bblBreakup(currentBbl);
 
-    const bblObjectToPush = this.store.createRecord('bbl', {
-      dcpBblnumber: currentBbl,
-      dcpDevelopmentsite: null,
-      dcpPartiallot: null,
+    // To avoid duplicate bbls
+    const projectBbls = await this.store.findAll('bbl');
+    const duplicateBbls = projectBbls.filter((bbl) => bbl.dcpBblnumber === currentBbl);
 
-      dcpUserinputborough: NUMERIC_BOROUGH_MAPPING[borough],
-      dcpUserinputblock: block,
-      dcpUserinputlot: lot,
+    if (duplicateBbls.length < 1) {
+      this.bblAlreadyCreated = false;
+      const bblObjectToPush = this.store.createRecord('bbl', {
+        dcpBblnumber: currentBbl,
+        dcpDevelopmentsite: null,
+        dcpPartiallot: null,
 
-      project: this.args.project,
-    });
+        dcpUserinputborough: NUMERIC_BOROUGH_MAPPING[borough],
+        dcpUserinputblock: block,
+        dcpUserinputlot: lot,
 
-    // set local variables for displaying address next to bbl in bbls list
-    const currentAddress = labsSearchResult.label;
-    const formattedAddress = currentAddress.replace(', New York, NY, USA', '');
-    bblObjectToPush.temporaryAddressLabel = formattedAddress;
+        project: this.args.project,
+      });
 
-    // use unshiftObect to push to TOP of array
-    // this sorting is so that when a user adds a new bbl, the bbl does not appear at the
-    // bottom of the screen where they will be unable to see it
-    this.args.bbls.unshiftObject(bblObjectToPush);
+      // set local variables for displaying address next to bbl in bbls list
+      const currentAddress = labsSearchResult.label;
+      const formattedAddress = currentAddress.replace(', New York, NY, USA', '');
+      bblObjectToPush.temporaryAddressLabel = formattedAddress;
+
+      // use unshiftObect to push to TOP of array
+      // this sorting is so that when a user adds a new bbl, the bbl does not appear at the
+      // bottom of the screen where they will be unable to see it
+      this.args.bbls.unshiftObject(bblObjectToPush);
+    } else {
+      this.bblAlreadyCreated = true;
+      this.duplicateBblNumber = currentBbl;
+    }
   }
 }
