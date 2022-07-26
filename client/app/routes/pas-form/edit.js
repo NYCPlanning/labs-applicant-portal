1b8d9397734b60da17347f6e6f537069dfb1af0a
horatio <horatiorosa@aol.com>
2022-07-26 16:59:30 -0400

    989 changed path of optionsets inport to relative path

--- client/app/routes/pas-form/edit.js
+++ client/app/routes/pas-form/edit.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import { STATUSCODE, STATECODE } from '../../optionsets/package';
+import { STATUSCODE, STATECODE } from 'client/optionsets/package';
 
 export default class PasFormEditRoute extends Route.extend(AuthenticatedRouteMixin) {
   @service router;
