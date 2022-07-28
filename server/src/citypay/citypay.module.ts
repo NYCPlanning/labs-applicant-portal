4a7f3db96186673211dbb3b712c666286a90b19f
godfreyyeung <godfreyyeung@gmail.com>
2022-07-28 11:21:24 -0400

    Spike building XML to CityPay

--- server/src/citypay/citypay.module.ts
+++ server/src/citypay/citypay.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '../config/config.module';
 import { CrmModule } from '../crm/crm.module';
+import { CityPayController } from './citypay.controller';
 import { CitypayService } from './citypay.service';
 
 @Module({
   imports: [ConfigModule, CrmModule],
   providers: [CitypayService],
+  controllers: [CityPayController],
   exports: [CitypayService]
 })
 export class CitypayModule {}
