98aa188b8712206c83fc69d74a7c648a61499158
godfreyyeung <godfreyyeung@gmail.com>
2022-08-17 16:20:06 -0400

    Remove xmlbuilder, use string literal

--- server/src/citypay/citypay.controller.ts
+++ server/src/citypay/citypay.controller.ts
@@ -11,8 +11,9 @@ import { AuthenticateGuard } from '../authenticate.guard';
 import { JsonApiDeserializePipe } from '../json-api-deserialize.pipe';
 import * as Request from 'request';
 import { pick } from 'underscore';
-import { create, fragment } from 'xmlbuilder2';
 import { first } from 'rxjs/operators';
+import axios from 'axios';
+import * as  url from 'url';
 
 const DCP_PROJECTINVOICE_CODES = {
   statuscode: {
@@ -38,50 +39,6 @@ export class CityPayController {
   }
 
   async createRequestXML(packageId) {
-    const retailPaymentRequestXML = `
-      <?xml version='1.0' encoding='UTF-8' standalone='yes'?>
-        <RetailPaymentRequest xmlns='${this.config.get('CITYPAY_DOMAIN')}'>
-          <profileID>${this.config.get('CITYPAY_USERNAME')}</profileID>
-          <profilePassword>${this.config.get('CITYPAY_PASSWORD')}</profilePassword>
-          <displayTransactionDescription_1></displayTransactionDescription_1>
-          <displayTransactionDescription_2></displayTransactionDescription_2>
-          <displayTransactionDescription_3></displayTransactionDescription_3>
-          <paymentExtraData></paymentExtraData>
-          <postBackURL>
-            ${this.config.get('CITYPAY_POSTBACK')}
-          </postBackURL>
-          <returnFromCartURL>
-            ${this.config.get('CITYPAY_RETURN_FROM_CART')}
-          </returnFromCartURL>
-          <returnFromCheckoutURL>
-            ${this.config.get('CITYPAY_RETURN_FROM_CHECKOUT')}
-          </returnFromCheckoutURL>
-          <agencyRequestID>
-            ${this.config.get('CITYPAY_REQUESTID')}
-          </agencyRequestID>
-        </RetailPaymentRequest>`;
-    const retailPaymentRequestLineItemsXML = `
-      <retailPaymentRequestLineItems xmlns='${this.config.get('CITYPAY_DOMAIN')}'>
-        <agencyIdentifier>${this.config.get('CITYPAY_AGENCYID')}</agencyIdentifier>
-        <displayLongDescription>City Environmental Quality Review Fees</displayLongDescription>
-        <displayShortDescription_1>City Environmental Quality Review Fees</displayShortDescription_1>
-        <displayShortDescription_2>Short Description 2</displayShortDescription_2>
-        <displayShortDescription_3></displayShortDescription_3>
-        <flexField_1>INV#123</flexField_1>
-        <flexField_2>FEE_TYPE1</flexField_2>
-        <flexField_3>PACKAGE_NAME1</flexField_3>
-        <itemCodeKey></itemCodeKey>
-        <transactionCode></transactionCode>
-        <lineItemExtraData>This is some extra line item data.</lineItemExtraData>
-        <paymentAmount>10.00</paymentAmount>
-        <quantity>1</quantity>
-        <sequenceNumber>1</sequenceNumber>
-        <unitPrice>10.00</unitPrice>
-      </retailPaymentRequestLineItems>`;
-
-    const paymentRequest = create(retailPaymentRequestXML);
-    const { retailPaymentRequestLineItems: lineItem } = create(retailPaymentRequestLineItemsXML).toObject() as any;
-
     const { records: [firstPackage] } = await this.crmService.get('dcp_packages', `
         $filter=dcp_packageid eq ${packageId}
         &$expand=dcp_dcp_package_dcp_projectinvoice_package(
@@ -91,16 +48,54 @@ export class CityPayController {
 
     const [ firstInvoice ] = firstPackage.dcp_dcp_package_dcp_projectinvoice_package;
 
-    lineItem.displayLongDescription = firstPackage.dcp_name;
-    lineItem.displayShortDescription_1 = firstPackage.dcp_packagetype;
-    lineItem.paymentAmount = firstInvoice.dcp_grandtotal;
-    lineItem.unitPrice = firstInvoice.dcp_grandtotal;
+    let getCartKey = null;
 
-    const lineItems = fragment({
-      retailPaymentRequestLineItems: lineItem
-    });
+    const finalPayload = `<RetailPaymentRequest xmlns="${this.config.get('CITYPAY_DOMAIN')}">
+          <profileID>${this.config.get('CITYPAY_USERNAME')}</profileID>
+          <profilePassword>${this.config.get('CITYPAY_PASSWORD')}</profilePassword>
+          <displayTransactionDescription_1></displayTransactionDescription_1>
+          <displayTransactionDescription_2></displayTransactionDescription_2>
+          <displayTransactionDescription_3></displayTransactionDescription_3>
+          <paymentExtraData></paymentExtraData>
+          <postBackURL>${this.config.get('CITYPAY_POSTBACK')}</postBackURL>
+          <returnFromCartURL>${this.config.get('CITYPAY_RETURN_FROM_CART')}</returnFromCartURL>
+          <returnFromCheckoutURL>${this.config.get('CITYPAY_RETURN_FROM_CHECKOUT')}</returnFromCheckoutURL>
+          <agencyRequestID>${this.config.get('CITYPAY_REQUESTID')}</agencyRequestID>
+          <retailPaymentRequestLineItems xmlns="${this.config.get('CITYPAY_DOMAIN')}">
+            <agencyIdentifier>${this.config.get('CITYPAY_AGENCYID')}</agencyIdentifier>
+            <displayLongDescription>${firstPackage.dcp_name}</displayLongDescription>
+            <displayShortDescription_1>${firstPackage.dcp_packagetype}</displayShortDescription_1>
+            <displayShortDescription_2>Filed EAS</displayShortDescription_2>
+            <displayShortDescription_3></displayShortDescription_3>
+            <flexField_1>XXXXXXXXXXX</flexField_1>
+            <flexField_2>CEQR</flexField_2>
+            <flexField_3>XXXXXXXXX_Filed EAS_1</flexField_3>
+            <itemCodeKey>XXXXXX</itemCodeKey>
+            <transactionCode>XXXXXX</transactionCode>
+            <lineItemExtraData>This is some extra line item data.</lineItemExtraData>
+            <paymentAmount>${firstInvoice.dcp_grandtotal}</paymentAmount>
+            <quantity>1</quantity>
+            <sequenceNumber>1</sequenceNumber>
+            <unitPrice>${firstInvoice.dcp_grandtotal}</unitPrice>
+          </retailPaymentRequestLineItems>
+        </RetailPaymentRequest>
+        `
 
-    paymentRequest.root().import(lineItems);
+    const params = new url.URLSearchParams({ saleData: finalPayload });
+
+    try {
+      getCartKey = await axios.post(`${this.config.get('PAYMENT_BASE_URL')}/${this.config.get('PAYMENT_STEP1_URL')}`, params.toString(), {
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded'
+        }
+      });
+
+      console.log("response: ", getCartKey);
+    } catch(e) {
+      console.log("error: ", e);
+    }
+
+    return getCartKey;
   }
 
   @Post('/getcartkey')
