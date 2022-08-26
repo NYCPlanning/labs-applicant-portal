e60b924a134a25cd7ba0ca5d8be6253195ec6f3e
godfreyyeung <godfreyyeung@gmail.com>
2022-08-26 01:54:50 -0400

    Generate cartkey for multi-invoice packages

--- server/src/citypay/citypay.module.ts
+++ server/src/citypay/citypay.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { InvoicesModule } from '../invoices/invoices.module';
 import { InvoicePostbackModule } from '../invoice-postback/invoice-postback.module';
 import { ConfigModule } from '../config/config.module';
 import { CrmModule } from '../crm/crm.module';
@@ -6,7 +7,7 @@ import { CityPayController } from './citypay.controller';
 import { CitypayService } from './citypay.service';
 
 @Module({
-  imports: [ConfigModule, CrmModule, InvoicePostbackModule],
+  imports: [ConfigModule, CrmModule, InvoicesModule, InvoicePostbackModule],
   providers: [CitypayService],
   controllers: [CityPayController],
   exports: [CitypayService]
