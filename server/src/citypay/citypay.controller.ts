cbbc1e0078d795660ff98ec89d1c6dd9753d6185
godfreyyeung <godfreyyeung@gmail.com>
2022-08-30 11:25:31 -0400

    8931 Add Auth Guard to CityPay ctrl

--- server/src/citypay/citypay.controller.ts
+++ server/src/citypay/citypay.controller.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/common';
 import { AuthenticateGuard } from '../authenticate.guard';
 
+@UseGuards(AuthenticateGuard)
 @Controller('citypay')
 export class CityPayController {
   @Post('/postbackpayment')
