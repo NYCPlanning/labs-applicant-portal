243537e70b0c2c5e0e67995464478cb96d24cabf
godfreyyeung <godfreyyeung@gmail.com>
2022-10-11 16:05:24 -0400

    10967 Restore invoices endpoint

--- server/src/invoices/invoices.module.ts
+++ server/src/invoices/invoices.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { CrmModule } from '../crm/crm.module';
 import { InvoicesService } from './invoices.service';
 import { ConfigModule } from '../config/config.module';
+import { InvoicesController } from './invoices.controller';
 
 @Module({
   imports: [ConfigModule, CrmModule],
   providers: [InvoicesService],
-  controllers: [],
+  controllers: [InvoicesController],
   exports: [InvoicesService]
 })
 export class InvoicesModule {}
