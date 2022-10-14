e0ddc345369e4537f3f5cce821e0c362192cbf12
godfreyyeung <godfreyyeung@gmail.com>
2022-10-14 14:57:19 -0400

    11029 Cast totalamount to Number

--- server/src/citypay/citypay.controller.ts
+++ server/src/citypay/citypay.controller.ts
@@ -150,13 +150,13 @@ export class CityPayController {
       for (let i = 0; i < lineItems.length; i += 1) {
         this.invoiceService.updateByName(lineItems[i].flexField1, {
           ...invoiceBody,
-          dcp_totalamountpaid: lineItems[i].amountPaid
+          dcp_totalamountpaid: Number(lineItems[i].amountPaid)
         });
       }
     } else {
       this.invoiceService.updateByName(lineItems.flexField1, {
         ...invoiceBody,
-        dcp_totalamountpaid: lineItems.amountPaid
+        dcp_totalamountpaid: Number(lineItems.amountPaid)
       });
     }
 
