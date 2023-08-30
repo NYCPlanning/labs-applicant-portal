9a24bb937974e6bd4c61a453228c0d19b635be77
horatio <horatiorosa@aol.com>
2023-08-29 20:59:44 -0400

    WIP no progress

--- client/app/components/saveable-form/field.js
+++ client/app/components/saveable-form/field.js
@@ -1,16 +1,15 @@
 import Component from '@glimmer/component';
-import { get } from '@ember/object';
 
 export default class SaveableFormFieldComponent extends Component {
   get value() {
-    console.log("this.args.data", this.args.data);
-    const value = this.args.data[this.args.attribute] ? this.args.data[this.args.attribute] : '';
-    console.log("getting value in field js", value ? value : "failed getting value");
-    return value;
+    // console.log('');
+    // console.log('getting value in field js', this.args.data[this.args.attribute] ? this.args.data[this.args.attribute] : 'failed getting value');
+    return this.args.data[this.args.attribute];
   }
 
   set value(newValue) {
-    console.log("setting value in field js", this.args.data[this.args.attribute] ? this.args.data[this.args.attribute] : "failed setting value");
+    // console.log('');
+    // console.log('setting value in field js', this.args.data[this.args.attribute] ? this.args.data[this.args.attribute] : 'failed setting value');
     this.args.data[this.args.attribute] = newValue;
   }
 
