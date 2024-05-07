23cefa78e9732c09cdd14fcf29e754ace11e000d
Tyler Matteo <tylermatteo434@gmail.com>
2024-05-07 10:14:56 -0400

    Revert "Remove code related to new Green Fast Track CEQR Type 2 eligibility f…" (#1229)
    This reverts commit 60a8ed96ce7c0e5a8e9593803b9a20dfd75041d3.

--- client/app/models/pas-form.js
+++ client/app/models/pas-form.js
@@ -717,6 +717,8 @@ export default class PasFormModel extends Model {
 
   @attr('number') dcpLanduseactiontype2;
 
+  @attr('number') dcpPursuetype2eligibility;
+
   @attr('number') dcpProposedprojectorportionconstruction;
 
   @attr('number') dcpProcommunityfacilitygrosssqft;
@@ -804,7 +806,9 @@ export default class PasFormModel extends Model {
 
   async saveDirtyBbls() {
     return Promise.all(
-      this.bbls.filter((bbl) => bbl.hasDirtyAttributes).map((bbl) => bbl.save())
+      this.bbls
+        .filter((bbl) => bbl.hasDirtyAttributes)
+        .map((bbl) => bbl.save()),
     );
   }
 
@@ -812,7 +816,7 @@ export default class PasFormModel extends Model {
     return Promise.all(
       this.applicants
         .filter((applicant) => applicant.hasDirtyAttributes)
-        .map((applicant) => applicant.save())
+        .map((applicant) => applicant.save()),
     );
   }
 
@@ -833,9 +837,7 @@ export default class PasFormModel extends Model {
   }
 
   get isApplicantsDirty() {
-    const dirtyApplicants = this.applicants.filter(
-      (applicant) => applicant.hasDirtyAttributes
-    );
+    const dirtyApplicants = this.applicants.filter((applicant) => applicant.hasDirtyAttributes);
 
     return dirtyApplicants.length > 0;
   }
