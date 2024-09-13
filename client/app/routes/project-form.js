df246428c9817b51a2185408578af3484f99c07d
tangoyankee <miller.tim108@gmail.com>
2024-09-13 16:36:18 -0400

    add incorrect model to project-form

--- client/app/routes/project-form.js
+++ client/app/routes/project-form.js
@@ -1,6 +1,20 @@
-import Route from '@ember/routing/route'
+import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import RSVP from 'rsvp';
 
-export default class ProjectForm extends Route.extend(AuthenticatedRouteMixin) {
+export default class ProjectFormRoute extends Route.extend(AuthenticatedRouteMixin) {
     authenticationRoute = '/';
+
+    async model(params) {
+        const projectFormPackage = await this.store.findRecord('package', params.id, {
+            reload: true,
+            include: [
+                'project.artifact'
+            ].join(),
+        })
+
+        return RSVP.hash({
+            package: projectFormPackage
+        })
+    }
 }
