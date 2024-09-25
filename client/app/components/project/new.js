4d9384ec7104757c913c5ca495a2d3291b86e2ce
tangoyankee <miller.tim108@gmail.com>
2024-09-25 15:48:18 -0400

    some basic UI

--- client/app/components/project/new.js
+++ client/app/components/project/new.js
@@ -1,6 +1,13 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from "@ember/object";
 
 export default class ProjectNewComponent extends Component {
-    @tracked _dcpProjectname;
+    @tracked _dcpProjectname = 'default project name';
+
+    @action
+    updateDcpProjectname (event) {
+        console.debug("dcp project change value", event.target.value);
+        this._dcpProjectname = event.target.value;
+    }
 }
