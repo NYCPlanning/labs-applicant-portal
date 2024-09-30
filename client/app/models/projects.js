a0098c6dab41b4888998fca877894e2ee2579fae
horatio <horatiorosa@aol.com>
2024-09-30 12:11:47 -0400

    removed thoughts of dirty packages

--- client/app/models/projects.js
+++ client/app/models/projects.js
@@ -6,19 +6,7 @@ export default class ProjectsModel extends Model {
 
   @attr('string') dcpProjectname;
 
-  // async save() {
-  //   await this.saveDirtyProject();
-  //   await super.save();
-  // }
-
-  // async saveDirtyProject() {
-  //   if (this.isProjectDirty) {
-  //     this.package.project.save();
-  //   }
-  // }
-
-  // get isProjectDirty() {
-  //   // return this.package.project.hasDirtyAttributes;
-  //   return true;
-  // }
+  async submit() {
+    await super.save();
+  }
 }
--- client/app/validations/submittable-project-form.js
+++ client/app/validations/submittable-project-form.js
@@ -1,16 +1,5 @@
-import {
-  validatePresence,
-} from 'ember-changeset-validations/validators';
 import SaveableProjectForm from './saveable-project-form';
-
 
 export default {
   ...SaveableProjectForm,
-  dcpProjectname: [
-    ...SaveableProjectForm.dcpProjectname,
-    validatePresence({
-      presence: true,
-      message: 'This field is required',
-    }),
-  ],
 };
