fb927382854a6f7eec9436cff93606d64ae1c032
horatio <horatiorosa@aol.com>
2024-10-01 11:32:07 -0400

    the validations are valid

--- client/app/components/packages/projects/new.js
+++ client/app/components/packages/projects/new.js
@@ -1,8 +1,8 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { SaveableProjectForm } from '../../../validations/saveable-project-form';
-import { SubmittableProjectForm } from '../../../validations/submittable-project-form';
+import SaveableProjectForm from '../../../validations/saveable-project-form';
+import SubmittableProjectForm from '../../../validations/submittable-project-form';
 
 export default class ProjectsNewFormComponent extends Component {
   validations = {
--- client/app/routes/projects/new.js
+++ client/app/routes/projects/new.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
 
-export default class ProjectsNewRoute extends Route {
+export default class ProjectsNewRoute extends Route.extend(AuthenticatedRouteMixin) {
   @service store;
 
   async model() {
