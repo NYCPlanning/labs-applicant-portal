7e2d1280a53cf8de25434c39f0c1737e93f5a93f
tangoyankee <miller.tim108@gmail.com>
2024-10-12 14:42:59 -0400

    Submit contact details
    Request the creation of a new contact if it does not exist
    
    closes #1248
    
    Co-authored-by: horatio <horatiorosa@aol.com>

--- client/app/components/packages/projects/new.js
+++ client/app/components/packages/projects/new.js
@@ -3,7 +3,6 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import SubmittableProjectsNewForm from '../../../validations/submittable-projects-new-form';
 
-
 export default class ProjectsNewFormComponent extends Component {
   validations = {
     SubmittableProjectsNewForm,
@@ -17,10 +16,49 @@ export default class ProjectsNewFormComponent extends Component {
 
   @action
   async submitPackage() {
+    const primaryContactInput = {
+      first: this.args.package.primaryContactFirstName,
+      last: this.args.package.primaryContactLastName,
+      email: this.args.package.primaryContactEmail,
+      phone: this.args.package.primaryContactPhone,
+      role: 'contact',
+    };
+
+    const applicantInput = {
+      first: this.args.package.applicantFirstName,
+      last: this.args.package.applicantLastName,
+      email: this.args.package.applicantEmail,
+      phone: this.args.package.applicantPhone,
+      role: 'applicant',
+    };
+
+    const contactInputs = [primaryContactInput, applicantInput];
     try {
-      await this.args.package.submit();
-    } catch (error) {
-      console.log('Save new project package error:', error);
+      const contactPromises = contactInputs
+        .map((contact) => this.store.queryRecord('contact',
+          {
+            email: contact.email,
+            includeAllStatusCodes: true,
+          }));
+
+      const contacts = await Promise.all(contactPromises);
+
+      const verifiedContactPromises = contacts.map((contact, index) => {
+        if (contact.id === '-1') {
+          const contactInput = contactInputs[index];
+          const contactModel = this.store.createRecord('contact', {
+            firstname: contactInput.first,
+            lastname: contactInput.last,
+            emailaddress1: contactInput.email,
+            telephone1: contactInput.phone,
+          });
+          return contactModel.save();
+        }
+        return contact;
+      });
+      await Promise.all(verifiedContactPromises);
+    } catch {
+      console.log('Save new project package error');
     }
   }
 }
--- client/app/validations/submittable-projects-new-form.js
+++ client/app/validations/submittable-projects-new-form.js
@@ -1,10 +1,109 @@
 import {
+  validateFormat,
   validateLength,
   validatePresence,
 } from 'ember-changeset-validations/validators';
 
 export default {
-  dcpProjectname: [
+  primaryContactFirstName: [
+    validateLength({
+      min: 0,
+      max: 50,
+      message: 'Text is too long (max {max} characters)',
+    }),
+    validatePresence({
+      presence: true,
+      message: 'This field is required',
+    }),
+  ],
+  primaryContactLastName: [
+    validateLength({
+      min: 0,
+      max: 50,
+      message: 'Text is too long (max {max} characters)',
+    }),
+    validatePresence({
+      presence: true,
+      message: 'This field is required',
+    }),
+  ],
+  primaryContactEmail: [
+    validateLength({
+      min: 0,
+      max: 50,
+      message: 'Text is too long (max {max} characters)',
+    }),
+    validatePresence({
+      presence: true,
+      message: 'This field is required',
+    }),
+    validateFormat({
+      type: 'email',
+      // Set allowBlank=true so that the validation message
+      // only appears after user first types something.
+      // This field still indicates it is 'required'
+      // through validatePresence
+      allowBlank: true,
+      message: 'Must be a valid email address',
+    }),
+  ],
+  primaryContactPhone: [
+    validateLength({
+      min: 0,
+      max: 10,
+      message: 'Text is too long (max {max} characters)',
+    }),
+  ],
+  applicantFirstName: [
+    validateLength({
+      min: 0,
+      max: 50,
+      message: 'Text is too long (max {max} characters)',
+    }),
+    validatePresence({
+      presence: true,
+      message: 'This field is required',
+    }),
+  ],
+  applicantLastName: [
+    validateLength({
+      min: 0,
+      max: 50,
+      message: 'Text is too long (max {max} characters)',
+    }),
+    validatePresence({
+      presence: true,
+      message: 'This field is required',
+    }),
+  ],
+  applicantEmail: [
+    validateLength({
+      min: 0,
+      max: 50,
+      message: 'Text is too long (max {max} characters)',
+    }),
+    validatePresence({
+      presence: true,
+      message: 'This field is required',
+    }),
+    validateFormat({
+      type: 'email',
+      // Set allowBlank=true so that the validation message
+      // only appears after user first types something.
+      // This field still indicates it is 'required'
+      // through validatePresence
+      allowBlank: true,
+      message: 'Must be a valid email address',
+    }),
+  ],
+  applicantPhone: [
+    validateLength({
+      min: 0,
+      max: 10,
+      message: 'Text is too long (max {max} characters)',
+    }),
+  ],
+  projectName: [
     validateLength({
       min: 0,
       max: 50,
