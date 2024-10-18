91838e8cb27a3724eae8fc29de28d0bebd87b7c1
horatio <horatiorosa@aol.com>
2024-10-18 11:16:27 -0400

    lint fixes

--- client/app/validations/submittable-projects-new-form.js
+++ client/app/validations/submittable-projects-new-form.js
@@ -1,7 +1,7 @@
 import {
   validateLength,
   validatePresence,
-  validateFormat
+  validateFormat,
 } from 'ember-changeset-validations/validators';
 
 export default {
@@ -9,34 +9,32 @@ export default {
     validateLength({
       min: 0,
       max: 50,
-      message: "Text is too long (max {max} characters)",
+      message: 'Text is too long (max {max} characters)',
     }),
     validatePresence({
       presence: true,
-      message: "This field is required",
+      message: 'This field is required',
     }),
   ],
   emailaddress1: [
     validateLength({
       min: 0,
       max: 50,
-      message: "Text is too long (max {max} characters)",
+      message: 'Text is too long (max {max} characters)',
     }),
     validatePresence({
       presence: true,
-      message: "This field is required",
+      message: 'This field is required',
     }),
     validateFormat({
-      type: "email",
+      type: 'email',
       // Set allowBlank=true so that the validation message
       // only appears after user first types sometihng.
       // This field still indicates it is 'required'
       // through validatePresence within
       // submittable-applicant-form
       allowBlank: true,
-      message: "Must be a valid email address",
+      message: 'Must be a valid email address',
     }),
   ],
 };
-
-
