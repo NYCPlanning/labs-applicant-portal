e8dddd79b7a0b9fa8abd977fc24e75b8c34b57d3
tangoyankee <miller.tim108@gmail.com>
2024-10-25 13:06:58 -0400

    correct lead_planner_format

--- server/src/projects/projects.controller.ts
+++ server/src/projects/projects.controller.ts
@@ -147,9 +147,7 @@ export class ProjectsController {
       'dcp_applicanttype': allowedAttrs['dcp_applicanttype'],
       'dcp_applicant_customer_contact@odata.bind': `/contacts(${allowedAttrs['_dcp_applicant_customer_value']})`,
       'dcp_applicantadministrator_customer_contact@odata.bind': `/contacts(${allowedAttrs['_dcp_applicantadministrator_customer_value']})`,
-      // 'dcp_leadplanner': null,
-      'dcp_leadplanner': allowedAttrs['_dcp_leadplanner_value'],
-      // 'dcp_leadplanner_contact@odata.bind': `/contacts(${allowedAttrs['_dcp_leadplanner_value']})`,
+      // 'dcp_leadplanner@odata.bind': `/systemusers(${guid})`,
     })
     return newProject;
   }
